9b7d8ffe8e2e333a955bfec1b403cfc185581264
sungjin-woo <enishi2001@gmail.com>
2017-11-13 12:07:11 +0900

    Create project
    - add gitignore
    - create project
    - create main view

---
+++ iOSContactRemover/iOSContactRemover/MainViewController.swift
@@ -0,0 +1,58 @@
+//
+//  MainViewController.swift
+//  iOSContactRemover
+//
+//  Created by enishi01 on 2017. 11. 13..
+//
+
+import UIKit
+import Contacts
+
+class MainViewController: UITableViewController {
+    
+    let store = CNContactStore()
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        self.requestAuthorization { [weak self] authorized in
+            guard authorized else {
+                let alert = UIAlertController(title: nil, message: "Can not access contact frameworks", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Confirm", style: .default, handler: nil))
+                self?.present(alert, animated: true, completion: nil)
+                return
+            }
+            
+            self?.showSourceListViewController()
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    private func showSourceListViewController() {
+        self.performSegue(withIdentifier: "show_source", sender: nil)
+    }
+    
+    private func requestAuthorization(completion: @escaping ((Bool) -> Void)) {
+        DispatchQueue.global(qos: .default).async { [weak self] in
+            let status = CNContactStore.authorizationStatus(for: .contacts)
+            switch status {
+                case .notDetermined:
+                    
+                    self?.store.requestAccess(for: .contacts, completionHandler: { value, error in
+                        DispatchQueue.main.async {
+                            completion(value)
+                        }
+                    })
+                case .authorized:
+                    DispatchQueue.main.async {
+                        completion(true)
+                    }
+                default:
+                    DispatchQueue.main.async {
+                        completion(false)
+                    }
+            }
+        }
+    }
+}
+
