0df58451accebe83b0e83100a0e7f0d2dd4ce983
sungjin-woo <enishi2001@gmail.com>
2017-11-13 14:35:58 +0900

    Implement delete groups

--- iOSContactRemover/iOSContactRemover/GroupsViewController.swift
+++ iOSContactRemover/iOSContactRemover/GroupsViewController.swift
@@ -17,20 +17,29 @@ class GroupsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let predicate = CNGroup.predicateForGroupsInContainer(withIdentifier: self.containerIdentifier)
-        do {
-            let groups = try self.store.groups(matching: predicate)
-            self.groups = groups
-        } catch {
-        }
+        self.loadGroups()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
+    // MARK: - IBAction
+    
     @IBAction func touchedUpInsideClose(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func touchedUpInsideDeelteAll(_ sender: Any) {
+        let alert = UIAlertController(title: nil, message: "All the groups in the container will be removed.\nContinue?", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Delete All", style: .destructive, handler: {
+            [weak self] action in
+            
+            self?.requestDeleteAllGroups()
+        }))
+        
+        self.present(alert, animated: true, completion: nil)
     }
 
     // MARK: - Table view data source
@@ -51,7 +60,56 @@ class GroupsViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if !self.isEditing && self.groups.count > indexPath.row {
+            let group = self.groups[indexPath.row]
+            let alert = UIAlertController(title: nil, message: "The group \"\(group.name)\" will be removed.\nContinue?", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            alert.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: {
+                [weak self] action in
+                
+                self?.requestDeleteGroups([group])
+            }))
+            
+            self.present(alert, animated: true, completion: nil)
+        }
         
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    // MARK: - Group
+    
+    private func loadGroups() {
+        let predicate = CNGroup.predicateForGroupsInContainer(withIdentifier: self.containerIdentifier)
+        do {
+            let groups = try self.store.groups(matching: predicate)
+            self.groups = groups
+        } catch {
+        }
+    }
+    
+    private func requestDeleteAllGroups() {
+        self.requestDeleteGroups(self.groups)
+    }
+    
+    private func requestDeleteGroups(_ groups: [CNGroup]) {
+        let request = CNSaveRequest()
+        
+        groups.forEach { group in
+            guard let grp = group.mutableCopy() as? CNMutableGroup else {
+                return
+            }
+            
+            request.delete(grp)
+        }
+        
+        do {
+            try self.store.execute(request)
+            print("Success, Groups are deleted : \(groups)")
+            
+            self.loadGroups()
+            self.tableView.reloadData()
+        } catch let e {
+            print("Failure, Groups delete error: \(e)")
+        }
     }
 }
