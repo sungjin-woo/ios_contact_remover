e4df0c4a7f26fd916abe4be168afe890df2200d5
sungjin-woo <enishi2001@gmail.com>
2017-11-13 14:09:14 +0900

    Implement contact delete

--- iOSContactRemover/iOSContactRemover/ContactsViewController.swift
+++ iOSContactRemover/iOSContactRemover/ContactsViewController.swift
@@ -32,14 +32,7 @@ class ContactsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let predicate = CNContact.predicateForContactsInContainer(withIdentifier: self.containerIdentifier)
-        do {
-            let contacts = try self.store.unifiedContacts(matching: predicate,
-                                                          keysToFetch: [CNContactIdentifierKey as CNKeyDescriptor,
-                                                                        CNContactFormatter.descriptorForRequiredKeys(for: .fullName)])
-            self.contacts = contacts
-        } catch {
-        }
+        self.loadContact()
     }
 
     override func didReceiveMemoryWarning() {
@@ -64,6 +57,19 @@ class ContactsViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        if self.contacts.count > indexPath.row {
+            let contact = self.contacts[indexPath.row]
+            let alert = UIAlertController(title: nil, message: "The contact \"\(contact.displayName)\" will be removed.\nContinue?", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            alert.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: {
+                [weak self] action in
+                
+                self?.requestDeleteContact(contact)
+            }))
+            
+            self.present(alert, animated: true, completion: nil)
+        }
      
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -76,5 +82,37 @@ class ContactsViewController: UITableViewController {
         }
         
         vc.containerIdentifier = self.containerIdentifier
+    }
+    
+    // MARK: - Contact
+    private func loadContact() {
+        let predicate = CNContact.predicateForContactsInContainer(withIdentifier: self.containerIdentifier)
+        do {
+            let contacts = try self.store.unifiedContacts(matching: predicate,
+                                                          keysToFetch: [CNContactIdentifierKey as CNKeyDescriptor,
+                                                                        CNContactFormatter.descriptorForRequiredKeys(for: .fullName)])
+            self.contacts = contacts
+        } catch {
+        }
+    }
+    
+    private func requestDeleteContact(_ contact: CNContact) {
+        
+        guard let deleteContact = contact.mutableCopy() as? CNMutableContact else {
+            return
+        }
+        
+        let request = CNSaveRequest()
+        request.delete(deleteContact)
+        
+        do {
+            try self.store.execute(request)
+            print("Success, Contact delete : \(contact)")
+            
+            self.loadContact()
+            self.tableView.reloadData()
+        } catch let e {
+            print("Failure, Contact delete error: \(e)")
+        }
     }
 }
